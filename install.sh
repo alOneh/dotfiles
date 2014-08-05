90f6f1227bb43589949b9e3226a6bb73f8f039b3
Hippolyte Alain <alain.hippolyte@gmail.com>
2014-08-05 22:01:15 +0200

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+NOCOLOR='\e[0m'
+REDCOLOR='\e[37;41m'
+
+if [[ $EUID -ne 0 ]]; then
+   echo -e "Run this bash script as root"
+    IS_ROOT=0
+else
+    IS_ROOT=1
+fi
+
+DOTFILES=`pwd`
+
+touch $DOTFILES/.private-gitconfig
+
+echo 'Create $HOME symlink'
+ln -sf $DOTFILES/.ackrc               ~/.ackrc
+ln -sf $DOTFILES/.gitconfig           ~/.gitconfig
+ln -sf $DOTFILES/.gitglobalexclude    ~/.gitglobalexclude
+ln -sf $DOTFILES/.psqlrc              ~/.psqlrc
+ln -sf $DOTFILES/.private-gitconfig   ~/.private-gitconfig
+ln -sf $DOTFILES/.tmux.conf           ~/.tmux.conf
+ln -sf $DOTFILES/.vimrc               ~/.vimrc
+if [[ ! -d ~/.git-template ]]; then
+    ln -sf $DOTFILES/git-template ~/.git-template
+fi
+
+if [[ $IS_ROOT = 1 ]]; then
+    if [[ `which php` ]] ; then
+        if [[ ! -f /usr/local/bin/composer ]]; then
+            echo 'Install composer'
+            curl -sS https://getcomposer.org/installer | php
+            mv composer.phar /usr/local/bin/composer
+        fi
+    fi
+fi
